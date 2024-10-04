07ee15605f34e2c20ce585a9428d1d92b1d558a9
Srikanth <srikanth.vutukuri58@gmail.com>
2024-10-04 08:02:46 -0500

    added new code

--- src/numbers.py
+++ src/numbers.py
@@ -11,3 +11,14 @@ print(color)
 print(type(color))
 print(you_like_python)
 print(type(you_like_python))
+
+
+age = 26
+height = 5.4
+color = "Green"
+you_like_python = False
+
+print(age, type(age))
+print(height, type(height))
+print(color, type(color))
+print(you_like_python, type(you_like_python))
