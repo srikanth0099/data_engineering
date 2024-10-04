6f952c263c6e7f6d956e2911203a370c224e3335
Srikanth <srikanth.vutukuri58@gmail.com>
2024-10-04 07:56:27 -0500

    assignment

---
+++ src/numbers.py
@@ -0,0 +1,13 @@
+age = 25
+height = 5.7
+color = "Red"
+you_like_python = True
+
+print(age)
+print(type(age))
+print(height)
+print(type(height))
+print(color)
+print(type(color))
+print(you_like_python)
+print(type(you_like_python))
