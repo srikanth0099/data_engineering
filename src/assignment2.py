b04156fd5480f9a6335727d7455a76775c2a2b4b
Srikanth <srikanth.vutukuri58@gmail.com>
2024-10-07 23:45:51 -0500

    reverse commit
    This reverts commit 57ec8c5c6e44f2c4343b91b66d00acbd916bf083.

--- src/assignment2.py
+++ src/assignment2.py
@@ -3,6 +3,7 @@
 # 1. len of string -> multiline SyntaxWarning
 # 2. assign a multi line variable string and split it based on " "
 # 3. concatinate first 5 words
+
 a = '''Data engineering involves the design, construction, and management of systems that collect, store, and analyze data at scale. 
     It encompasses tasks such as data extraction, transformation, loading (ETL), and ensuring that data is structured, clean, 
     and available for analysis or machine learning.'''
